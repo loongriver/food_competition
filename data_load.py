a6fa7e1e3096a7eb35127acc9d5f2934d6a5bc1e
long <805391603@qq.com>
2019-04-01 21:36:19 +0800

    修改

--- data_load.py
+++ data_load.py
@@ -7,11 +7,30 @@ class data_reader(object):
 		self.data_path = data_path
 		self.df = pd.read_csv(self.data_path,sep = '\t', header = 'infer', engine = 'python') 
 
+	def process_data(self):
+		# 获取文本数据
+		titles = df['Title']
+		abstract = df['abstract']
+
+
 	def show_data(self):
 		print(self.df[0:1])
 
 		# 检查缺失值： eventID 和 abstract 有缺失
 		print(self.df.isnull().any())
+
+	def cleanText(self, corpus):
+		#对英文做简单的数据清洗预处理，
+		punctuation = """.,?!:;(){}[]"""
+		corpus = [z.lower().replace('\n','') for z in corpus]
+        corpus = [z.replace('<br />', ' ') for z in corpus]
+
+        #treat punctuation as individual words
+        for c in punctuation:
+            corpus = [z.replace(c, ' %s '%c) for z in corpus]
+        corpus = [z.split() for z in corpus]
+        return corpus
+
 
 data_path = "Training Set for Competition.txt"
 data = data_reader(data_path)
---
+++ model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf 
+
+
+class model(object):
+	"""docstring for model"""
+	def __init__(self, arg):
+		super(model, self).__init__()
+		self.arg = arg
